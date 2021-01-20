25969402e4fb2c7fef7d5c746c4f762de358b24d
Edex13 <ederson12f@gmail.com>
2021-01-19 21:07:06 -0500

    vista movil terminada , con carrusel en glider

--- carousel.js
+++ carousel.js
@@ -1,14 +1,31 @@
 window.addEventListener('load', function () {
    if (screen.width > 1000) {
-      new Glider(document.querySelector('.carousel__container'), {
+      new Glider(document.querySelector('.carousel__container--miLista'), {
          slidesToShow: 4,
          slidesToScroll: 4,
-         dots: '.indicadores',
+         dots: '.indicadores--miLista',
          arrows: {
-            prev: '.btn__izq',
-            next: '.btn__derec',
+            prev: '.btn__izq--miLista',
+            next: '.btn__derec--miLista',
          },
       });
-      var carousel = document.querySelector('.carousel');
+      new Glider(document.querySelector('.carousel__container--tendencia'), {
+         slidesToShow: 4,
+         slidesToScroll: 4,
+         dots: '.indicadores--tendencia',
+         arrows: {
+            prev: '.btn__izq--tendencia',
+            next: '.btn__derec--tendencia',
+         },
+      });
+      new Glider(document.querySelector('.carousel__container--originalesDePlatziVideo'), {
+         slidesToShow: 4,
+         slidesToScroll: 4,
+         dots: '.indicadores--originalesDePlatziVideo',
+         arrows: {
+            prev: '.btn__izq--originalesDePlatziVideo',
+            next: '.btn__derec--originalesDePlatziVideo',
+         },
+      });
    }
 });
