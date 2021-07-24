06f73e77561aba6d17bb26d2d997bd7707eebbf1
ederson lopez <ederson12f@gmail.com>
2021-07-23 23:26:05 -0500

    Implementacion del estado y las hooks del componente carouselItem

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,28 +4,36 @@ import Searcher from '../components/Searcher';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import useInitialState from "../hooks/useInitialState";
+import useInitialState from '../hooks/useInitialState';
 const App = () => {
-	const API = 'http://localhost:8080/initialState'
-	const initialState =useInitialState(API);
-	console.log(initialState);
+	const API = 'http://localhost:8080/initialState';
+	const initialState = useInitialState(API);
+	console.log(initialState.mylist);
 	return (
 		<React.Fragment>
 			<Header />
 			<Searcher />
-			<Categories title="Mi lista">
-				<Carousel>
-					<CarouselItem />
-				</Carousel>
-			</Categories>
+			{initialState.mylist?.length > 0 && (
+				<Categories title="Mi lista">
+					<Carousel>
+						{initialState.mylist?.map((item) => (
+							<CarouselItem key={item.id} {...item} />
+						))}
+					</Carousel>
+				</Categories>
+			)}
 			<Categories title="Tendencia">
 				<Carousel>
-					<CarouselItem />
+					{initialState.trends?.map((item) => (
+						<CarouselItem key={item.id} {...item} />
+					))}
 				</Carousel>
 			</Categories>
 			<Categories title="Originales De Platzi Video">
 				<Carousel>
-					<CarouselItem />
+					{initialState.originals?.map((item) => (
+						<CarouselItem key={item.id} {...item} />
+					))}
 				</Carousel>
 			</Categories>
 		</React.Fragment>
