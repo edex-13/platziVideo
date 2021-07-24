cecd79e868130fdd24bc62eb53785e02f84814a7
ederson lopez <ederson12f@gmail.com>
2021-07-23 23:11:41 -0500

    Fake API y hooks para hacer la peticion a dicha APi

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,7 +3,7 @@ import React, { Children } from 'react';
 import '../assets/styles/components/Categories.scss'
 const Categories = ({children, title}) => (
   <div className="categories">
-    <h2 class="categories__title">{title}</h2>
+    <h2 className="categories__title">{title}</h2>
     {children}
   </div>  
 )
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../components/Header';
 import Searcher from '../components/Searcher';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-const App = () => (
-	<React.Fragment>
-		<Header />
-		<Searcher/>
-		<Categories title="Mi lista">
-			<Carousel>
-				<CarouselItem/>
-				<CarouselItem/>
-				<CarouselItem/>
-				<CarouselItem/>
-				<CarouselItem/>
-				<CarouselItem/>
-				<CarouselItem/>
-				<CarouselItem/>
-				<CarouselItem/>
-			</Carousel>
-		</Categories>
-	</React.Fragment>
-);
+import useInitialState from "../hooks/useInitialState";
+const App = () => {
+	const API = 'http://localhost:8080/initialState'
+	const initialState =useInitialState(API);
+	console.log(initialState);
+	return (
+		<React.Fragment>
+			<Header />
+			<Searcher />
+			<Categories title="Mi lista">
+				<Carousel>
+					<CarouselItem />
+				</Carousel>
+			</Categories>
+			<Categories title="Tendencia">
+				<Carousel>
+					<CarouselItem />
+				</Carousel>
+			</Categories>
+			<Categories title="Originales De Platzi Video">
+				<Carousel>
+					<CarouselItem />
+				</Carousel>
+			</Categories>
+		</React.Fragment>
+	);
+};
 export default App;
