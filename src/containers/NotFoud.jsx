8a3f6245b4cd9031a4b27bc2bf5016591f900096
ederson lopez <ederson12f@gmail.com>
2021-07-24 22:36:52 -0500

    Component NotFoud terminado

---
+++ src/containers/NotFoud.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/components/NotFoud.scss';
+
+const NotFound = () => (
+	<section className="container">
+		<div className="container--title">
+			<h1>404</h1>
+		</div>
+		<p className="container--details">Página no encontrada</p>
+	</section>
+);
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./containers/Register"
+import App from "./containers/NotFoud"
 
 // Estilos
 
