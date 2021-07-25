9137202e03e7e8dfee0b607c4e4526e32b82178f
ederson lopez <ederson12f@gmail.com>
2021-07-24 23:03:42 -0500

    Route de notFoud

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,12 +3,14 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import App from '../containers/App';
 import Login from '../containers/Login';
 import Register from '../containers/Register';
+import NotFound from '../containers/NotFoud';
 const Routes = () => (
 	<BrowserRouter>
 		<Switch>
 			<Route exact path="/" component={App} />
 			<Route exact path="/login" component={Login} />
 			<Route exact path="/register" component={Register} />
+			<Route component={NotFound} />
 		</Switch>
 	</BrowserRouter>
 );
