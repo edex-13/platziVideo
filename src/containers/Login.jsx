6e5c88edb36be8ce915a6da222dfb5db57bc4204
ederson lopez <ederson12f@gmail.com>
2021-07-24 23:16:33 -0500

    uso de liks para la navegacion basica de la pagina

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import icon_google from '../assets/images/google-icon.png';
 import icon_twitter from '../assets/images/twitter-icon.png';
 import '../assets/styles/components/Login.scss';
@@ -27,7 +28,7 @@ const Login = () => (
 				</div>
 			</section>
 			<p className="login__container--register">
-				No tienes ninguna cuenta <a href="">Regístrate</a>
+				No tienes ninguna cuenta <Link to="/register">Regístrate</Link>
 			</p>
 		</section>
 	</section>
